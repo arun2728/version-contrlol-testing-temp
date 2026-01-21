489e76c1ff71d7e6e7f6bc8b943bf1d327830043
Arun Addagatla <aruna@lamatic.ai>
2026-01-21 17:01:49 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/AbleEaster.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "b8c48754-0479-4600-928a-6651d446c051",
+  "name": "Able Easter",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2026-01-21T11:31:47.757175+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
