9f4106c6fef280dfced450cb9b88309de3f21c54
Arun Addagatla <aruna@lamatic.ai>
2026-03-21 14:07:02 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/PitifulEventide.ts
@@ -0,0 +1,88 @@
+const flowConfig = {
+  "id": "f989c3bc-da3c-4021-8b19-f527920ba0a4",
+  "name": "Pitiful Eventide",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {
+        "modes": {},
+        "nodeId": "graphqlNode",
+        "values": {
+          "nodeName": "API Request",
+          "responeType": "realtime",
+          "advance_schema": "{\"sampleInput\":\"string\"}"
+        },
+        "trigger": true
+      },
+      "type": "triggerNode",
+      "measured": {
+        "width": 216,
+        "height": 93
+      },
+      "position": {
+        "x": 0,
+        "y": 0
+      },
+      "selected": false
+    },
+    {
+      "id": "responseNode_triggerNode_1",
+      "data": {
+        "label": "Response",
+        "nodeId": "graphqlResponseNode",
+        "values": {
+          "headers": "{\"content-type\":\"application/json\"}",
+          "retries": "0",
+          "nodeName": "API Response",
+          "webhookUrl": "",
+          "retry_delay": "0",
+          "outputMapping": "{}"
+        },
+        "isResponseNode": true
+      },
+      "type": "responseNode",
+      "measured": {
+        "width": 216,
+        "height": 93
+      },
+      "position": {
+        "x": 0,
+        "y": 130
+      }
+    }
+  ],
+  "edges": [
+    {
+      "id": "triggerNode_1-responseNode_triggerNode_1-997",
+      "type": "defaultEdge",
+      "source": "triggerNode_1",
+      "target": "responseNode_triggerNode_1",
+      "sourceHandle": "bottom",
+      "targetHandle": "top"
+    },
+    {
+      "id": "response-trigger_triggerNode_1",
+      "type": "responseEdge",
+      "source": "triggerNode_1",
+      "target": "responseNode_triggerNode_1",
+      "sourceHandle": "to-response",
+      "targetHandle": "from-trigger"
+    }
+  ],
+  "status": "active",
+  "created_at": "2026-03-05T05:38:16.330116+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
