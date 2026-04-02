c3a1679337abec5b408d4899db462200a188559a
Arun Addagatla <aruna@lamatic.ai>
2026-04-02 17:10:27 +0530

    [LAMATIC-COMMIT] Created flow

---
+++ lamatic/flows/FitSecretary.ts
@@ -0,0 +1,32 @@
+const flowConfig = {
+  "id": "32d28a90-259f-4cce-a337-f33fa0de2448",
+  "name": "Fit Secretary",
+  "nodes": [
+    {
+      "id": "triggerNode_1",
+      "data": {},
+      "type": "triggerNode",
+      "position": {
+        "x": 0,
+        "y": 0
+      }
+    }
+  ],
+  "edges": [],
+  "status": "active",
+  "created_at": "2026-04-02T11:40:25.809834+00:00"
+};
+
+export async function getNodesAndEdges(): Promise<{
+    nodes: Record<string, any>[],
+    edges: Record<string, any>[],
+}> {
+    return {
+        nodes: flowConfig.nodes,
+        edges: flowConfig.edges,
+    }
+}
+
+export async function getFlowConfig(): Promise<Record<string, any>> {
+    return flowConfig;
+}
